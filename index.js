302f964a7f50c6194c99b1c5cce888922222313d
Shahde'Ann Nicholas <>
2021-04-27 19:29:24 +0000

    complete

--- index.js
+++ index.js
@@ -15,8 +15,25 @@ in the object.
 function averageBalance(array){
   //CODE BELOW HERE
 
+  
+ let totalSum = 0;
 
+
+
+
+
+
+
+
+for(var i = 0; i <= array.length - 1; i++){
+console.log(parseFloat(array[i].balance.replace('$', '').replace(',', '')))
+
+
+
+}
 
 
   //CODE ABOVE HERE
 }
+
+console.log(averageBalance(customers))
